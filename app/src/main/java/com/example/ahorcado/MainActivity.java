5bc8d0d0e5a7c83634ba0d3ef4ea7e3517db8d73
GerardoDM <geradelgado22@hotmail.com>
2020-04-21 18:45:30 -0500

    diseño basico

---
+++ app/src/main/java/com/example/ahorcado/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.ahorcado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+
+}
