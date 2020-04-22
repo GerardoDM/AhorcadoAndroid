356e466b1bae01b2b5fa86127a6665ee9f3ef2bb
GerardoDM <geradelgado22@hotmail.com>
2020-04-21 19:31:27 -0500

    metodos iniciar y mostrar palabra

--- app/src/main/java/com/example/ahorcado/MainActivity.java
+++ app/src/main/java/com/example/ahorcado/MainActivity.java
@@ -15,20 +15,75 @@ import java.util.Collections;
 public class MainActivity extends AppCompatActivity {
 
     TextView txtPalabraParaSerAdivinada;
-    String PalabraParaSerAdivinada;
+    String palabraParaSerAdivinada;
     String palabraMostradaString;
     char [] palabraMostradaCharArray;
     ArrayList<String> listaPalabras;
     EditText editInput;
     TextView txtLetraIntentada;
     String letraIntentada;
-    final String MSJ_WITH_LETRAS_INTENTADAS = "Letras intentadas: ";
+    final String MSJ_CON_LETRAS_INTENTADAS = "Letras intentadas: ";
     TextView txtIntentosRestantes;
     String intentosRestantes;
     final String MSJ_GANADOR = "Ganaste";
     final String MSJ_PERDEDOR = "Perdiste";
 
+    void revealLetterInWord(char letter){
+        int indexOfLetter = palabraParaSerAdivinada.indexOf(letter);
 
+        while(indexOfLetter >= 0){
+            palabraMostradaCharArray[indexOfLetter] = palabraParaSerAdivinada.charAt(indexOfLetter);
+            indexOfLetter = palabraParaSerAdivinada.indexOf(letter,indexOfLetter + 2);
+
+        }
+
+        palabraMostradaString = String.valueOf(palabraMostradaCharArray);
+
+
+
+    }
+
+    void displayWordOnScreen(){
+        String formattedString = "";
+        for(char character : palabraMostradaCharArray){
+            formattedString += character + "";
+        }
+
+        txtPalabraParaSerAdivinada.setText(formattedString);
+    }
+
+    void iniciar() {
+        Collections.shuffle(listaPalabras);
+        palabraParaSerAdivinada = listaPalabras.get(0);
+        listaPalabras.remove(0);
+
+        //inicializar array de caracteres
+        palabraMostradaCharArray = palabraParaSerAdivinada.toCharArray();
+
+        //agregar guion bajo
+        for (int i = 1; i < palabraMostradaCharArray.length - 1; i++) {
+            palabraMostradaCharArray[i] = '_';
+        }
+
+        revealLetterInWord(palabraMostradaCharArray[0]);
+
+        revealLetterInWord(palabraMostradaCharArray[palabraMostradaCharArray.length - 1]);
+
+        palabraMostradaString = String.valueOf(palabraMostradaCharArray);
+
+        displayWordOnScreen();
+
+        editInput.setText("");
+
+        letraIntentada = "";
+
+        txtLetraIntentada.setText(MSJ_CON_LETRAS_INTENTADAS);
+
+        intentosRestantes = " X X X X X";
+
+        txtIntentosRestantes.setText(intentosRestantes);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
